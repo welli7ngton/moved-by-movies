fbc8a0ca24db55915670d0efffb9381008133147
welli7ngton <wellingtonasilva45@gmail.com>
2024-03-29 19:43:38 -0300

    Initial commit

---
+++ movedbymovies/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # cria e configura o app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABESE=os.path.join(app.instance_path, 'database.sqlite'),
+    )
+
+    if test_config is None:
+        # carrega a instancia config, se existir, quando não está em cenário
+        # de testes
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return "Olaaaa mundo"
+
+    return app
