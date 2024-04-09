d883d3f031eb13fd7ec3ece0fd1ce9c0dd1cdc0b
welli7ngton <wellingtonasilva45@gmail.com>
2024-04-08 22:31:50 -0300

    test: test user register post, and redirecting user if he uses a weak password to a json exception

---
+++ tests/test_auth.py
@@ -0,0 +1,65 @@
+import requests
+
+
+def test_register_post():
+    data_json = {
+        'username': 'test',
+        'email': 'testing1@test.com',
+        'password': '1AmStr0ng!',
+        'confirm-password': '1AmStr0ng!',
+        'birth': '2000-01-01'
+    }
+    res = requests.post('http://127.0.0.1:5000/auth/register', data=data_json)
+    assert res.status_code == 200
+
+
+def test_register_post_get_an_PasswordHasNoPunctuation_error():
+    data_json = {
+        'username': 'test',
+        'email': 'testing1@test.com',
+        'password': '1AmStr0ng',
+        'confirm-password': '1AmStr0ng',
+        'birth': '2000-01-01'
+    }
+    res = requests.post('http://127.0.0.1:5000/auth/register', data=data_json)
+    assert res.json() == {"error": "Password has no punctuation(!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~)"}
+    assert res.status_code == 200
+
+
+def test_register_post_get_an_HasNoNumbers_error():
+    data_json = {
+        'username': 'test',
+        'email': 'testing1@test.com',
+        'password': 'IAmStrOng!',
+        'confirm-password': 'IAmStrOng!',
+        'birth': '2000-01-01'
+    }
+    res = requests.post('http://127.0.0.1:5000/auth/register', data=data_json)
+    assert res.json() == {"error": "Password has no numbers (0123456789)"}
+    assert res.status_code == 200
+
+
+def test_register_post_get_an_HasNotEnoughLength_error():
+    data_json = {
+        'username': 'test',
+        'email': 'testing1@test.com',
+        'password': '1!Qa',
+        'confirm-password': '1!Qa',
+        'birth': '2000-01-01'
+    }
+    res = requests.post('http://127.0.0.1:5000/auth/register', data=data_json)
+    assert res.json() == {"error": "Password has no enough length(8)"}
+    assert res.status_code == 200
+
+
+def test_register_post_get_an_HasNoUppercase_error():
+    data_json = {
+        'username': 'test',
+        'email': 'testing1@test.com',
+        'password': '1!qazwsx',
+        'confirm-password': '1!qazwsx',
+        'birth': '2000-01-01'
+    }
+    res = requests.post('http://127.0.0.1:5000/auth/register', data=data_json)
+    assert res.json() == {"error": "Password has no uppercase(ABCDEFGHIJKLMNOPQRSTUVWXYZ)"}
+    assert res.status_code == 200
