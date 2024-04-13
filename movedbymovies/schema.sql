db51af987bbc86d56f3dda65e449914a765a58cc
welli7ngton <wellingtonasilva45@gmail.com>
2024-04-13 20:21:47 -0300

    refactor: remove the 'user_mocvies' and replace the 'totalSeasons' field for 'writer' to fix a bug in register movies

--- movedbymovies/schema.sql
+++ movedbymovies/schema.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS user_movies;
 DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS purchases;
 DROP TABLE IF EXISTS admin;
@@ -19,13 +18,6 @@ CREATE TABLE users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE user_movies (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER NOT NULL,
-  movie_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (movie_id) REFERENCES movies(id)
-);
 
 CREATE TABLE movies (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -37,7 +29,7 @@ CREATE TABLE movies (
   actors TEXT NULL,
   director TEXT NULL,
   poster TEXT NULL,
-  totalSeasons TEXT NULL,
+  writer TEXT NULL,
   country TEXT NULL,
   language TEXT NULL,
   awards TEXT NULL,
@@ -61,6 +53,6 @@ CREATE TABLE admin (
 );
 
 INSERT INTO users(username, email, password)
-VALUES("welli7ngton", "wellington@admin.com", "scrypt:32768:8:1$lKJvDyRQRJKfJ6Me$6e870a499da612c9b9f501287e0f920fd307d721fa9da4f97a98298bbf522e9060c74e8447c4b39c46882bdcbeaab328b8b109225a131778817414c82e57decf");
+VALUES("admin", "admin", "scrypt:32768:8:1$lKJvDyRQRJKfJ6Me$6e870a499da612c9b9f501287e0f920fd307d721fa9da4f97a98298bbf522e9060c74e8447c4b39c46882bdcbeaab328b8b109225a131778817414c82e57decf");
 
 INSERT INTO admin(user_id) VALUES(1);
