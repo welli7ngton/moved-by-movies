cca02575bc75f2a7a25df876060bb7142e220e06
welli7ngton <wellingtonasilva45@gmail.com>
2024-04-14 23:42:18 -0300

    refactor: remove the view register movie from this file, because that function shoud be used only by admins

--- movedbymovies/movies.py
+++ movedbymovies/movies.py
@@ -1,8 +1,4 @@
 # flake8: noqa
-import os
-from dotenv import load_dotenv
-import requests
-
 from flask import (
     Blueprint, flash, render_template, request
 )
@@ -10,55 +6,8 @@ from flask import (
 from movedbymovies.db import get_db
 from movedbymovies.auth import login_required
 
-load_dotenv()
 
 bp = Blueprint('movies', __name__, url_prefix='/movies')
-
-API_KEY = os.getenv('API_KEY')
-HOST = 'https://www.omdbapi.com/'
-
-
-@bp.route('/register', methods=('GET', 'POST'))
-@login_required
-def register():
-    if request.method == 'POST':
-        title = request.form['movie-title']
-
-        db = get_db()
-        error = None
-
-        if not title:
-            error = 'Movie Title is required.'
-
-
-        if error is None:
-            response = requests.get(f'{HOST}?t={title}&apikey={API_KEY}&plot=full')
-            
-            title = response.json()
-
-            try:
-                db.execute(
-                    """INSERT INTO movies
-                        (title, plot, released, runtime,
-                        gender, director, poster, imdbRating,
-                        actors, awards, writer, country,
-                        language)
-                    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)""",
-                    (
-                        title['Title'], title['Plot'], title['Released'],
-                        title['Runtime'], title['Genre'], title['Director'],
-                        title['Poster'], title['imdbRating'],
-                        title['Actors'], title['Awards'],
-                        title['Writer'], title['Country'],
-                        title['Language']
-                    ),
-                )
-                db.commit()
-                flash('Movie Registered!')
-            except KeyError as e:
-                flash('Movie not Found :(')
-
-    return render_template('movies/register_movie.html')
 
 
 @bp.route('/catalog', methods=('GET','POST'))
