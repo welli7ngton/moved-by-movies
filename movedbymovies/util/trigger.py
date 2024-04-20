4581397aac849f954f4c84d0a9fb3d38c56639b1
welli7ngton <wellingtonasilva45@gmail.com>
2024-04-19 21:22:44 -0300

    refactor: add a admin_id field instead of use the admin username, receive the date from the date python funcion defined in the trigger and remove the deleted_username field, create a new trigger to the register_movie

--- movedbymovies/util/trigger.py
+++ movedbymovies/util/trigger.py
@@ -1,13 +1,26 @@
-# flake8:noqa
-from typing import Any
 from datetime import date
+from typing import Any
 
 
 class Trigger:
     @classmethod
-    def make_log(cls, motivation: str, username: str, deleted_user: str, db):
+    def delete(cls, admin_id: int, motivation: str, db: Any):
+
         db.execute(
-            'INSERT INTO log_deletes (delete_date, username, motivation, deleted_username) VALUES (?,?,?,?)',
-            (date.today(), username, motivation, deleted_user)
+            """INSERT INTO logs
+            (type_operation, admin_id, motivation, delete_date)
+            VALUES (?,?,?,?,?)""",
+            ('Delete', admin_id, motivation, date.today())
+        )
+        db.commit()
+
+    @classmethod
+    def movie_registered(cls, admin_id: int, db: Any):
+        db.execute(
+            """INSERT INTO logs
+            (type_operation, admin_id,
+            motivation, delete_date)
+            VALUES (?,?,?,?,?)""",
+            ('Movie registered', admin_id, 'movie added', date.today())
         )
         db.commit()
